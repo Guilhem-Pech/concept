ff22d82528bedaaa5d95aa665c7494c6a8a87e65
Guilhem PECH <guilhempech@gmail.com>
2018-03-03 09:31:38 +0100

    Some progress

--- js/plateau.js
+++ js/plateau.js
@@ -23,25 +23,83 @@ class Concept {
     constructor(image, parent) {
         self = this;
         this.image = image;
-        this.htmlObject = $('<img />').attr('src', this.image).addClass("img-fluid mr-1 mb-1").css({
-            "max-width": "5.5%"
+        this.htmlImage = $('<img />').attr('src', this.image).addClass("img-fluid mr-1 mb-1").css({});
+
+        this.htmlObject = $("<div/>").addClass("imagecontainer").append(this.htmlImage).css({
+            width: "5.5%",
+            height: "auto",
+            display: "flex",
+            margin: "2px",
+            "align-items": "center",
+            "justify-content": "center",
+
         });
         
         this.htmlparent = $(parent).append(this.htmlObject);
         
         this.htmlObject.hover(function(){
-            $(this).css({
+            $(this).children().css({
                 "transform":"scale(2)",
                 "transition":"250ms ease-in-out",
                 "z-index":1
+            });
+            $(this).children(".jeton").css({
+                opacity: 0.5
             })
-        }, function(){ 
-            $(this).css({
+
+        }, function () {
+            $(this).children().prop("z-index", function (val, old) {
+                return old - 1;
+            }).css({
                 "transform":"scale(1)",
                 "z-index": 0
+            });
+            $(this).children(".jeton").css({
+                opacity: 1
             })
-        });    
-        // this.htmlObject.droppable();
+
+        });
+        this.htmlObject.droppable({
+            accept: function () {
+                if ($(this).children(".jeton").length > 0)
+                    return false;
+                return true;
+            },
+            drop: function (event, ui) {
+                let draggable = ui.draggable;
+                // draggable.attr("src");
+                let child = new Jeton(draggable.attr("src"), this);
+                child.htmlObject.attr({
+                    "data-container": "body",
+                    "data-toggle": "popover",
+                });
+                child.htmlObject.css({
+                    position: "absolute",
+                    "width": "3%",
+                });
+                child.disableDrag();
+
+                let dismiss = $("<bouton/>").addClass("btn btn-success").text("Abort").attr("type", "button").css("margin-left", "2px");
+                let remove = $("<bouton/>").addClass("btn btn-danger").text("Remove").attr("type", "button");
+
+                dismiss.on("click", function () {
+                    child.htmlObject.popover("hide");
+                });
+
+                remove.on("click", function () {
+                    child.htmlObject.popover("hide");
+                    child.htmlObject.remove();
+                });
+                child.htmlObject.popover({
+                    content: remove.add(dismiss),
+                    html: true,
+                    title: "Do you want to remove the token ?"
+                });
+                child.htmlObject.on("click", function () {
+                    child.htmlObject.popover();
+                })
+            }
+        });
     }
 }
 
@@ -63,7 +121,7 @@ class Jeton {
     constructor(image, parent) {
         self = this;
         this.image = image;
-        this.htmlObject = $('<img />').attr('src', this.image).addClass("img-fluid mr-1 mb-1").css({
+        this.htmlObject = $('<img />').attr('src', this.image).addClass("img-fluid mr-1 mb-1 jeton").css({
             "max-width": "5%",
             cursor: "move",
             "z-index" : 2
@@ -74,7 +132,12 @@ class Jeton {
         this.htmlObject.draggable({
             revert: "invalid",
             stack: ".draggable",
+            containment: "#dragzone",
             helper: 'clone'
         });
+        this.disableDrag = function () {
+            self.htmlObject.draggable('disable');
+        }
     }
+
 }
